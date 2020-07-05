1668d6a468050486e4c2a7aa7bd0d3559b141f3b
ryunosuke <kr07031321@gmail.com>
2020-07-06 01:03:13 +0900

    firebase firestore 値の削除 ダイアログの出し方と簡略化（関数で）
    firebase Authを入れたのとsignupディレクトリ作成
    ”KBOYのFlutter大学”

--- lib/presentation/book_list/book_list_page.dart
+++ lib/presentation/book_list/book_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:youtubeapp/add_book/add_book_page.dart';
+import 'package:youtubeapp/presentation/add_book/add_book_page.dart';
+import 'package:youtubeapp/domain/book.dart';
 
 import 'book_list_model.dart';
 
@@ -38,6 +39,28 @@ class BookListPage extends StatelessWidget {
                       model.fetchBooks();
                     },
                   ),
+                  onLongPress: () async {
+                    //TODO:ここで削除の処理
+                    await showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('${book.title}を削除しますか?'),
+                            actions: <Widget>[
+                              FlatButton(
+                                child: Text('OK'),
+                                onPressed: () async {
+                                  Navigator.of(context).pop();
+
+                                  //TODO: 削除のAPIを叩く
+                                  //TODO: deleteBookの引数でcontext, model, bookを渡してあげる
+                                  await deleteBook(context, model, book);
+                                },
+                              ),
+                            ],
+                          );
+                        });
+                  },
                 ),
               )
               .toList();
@@ -64,6 +87,39 @@ class BookListPage extends StatelessWidget {
           );
         }),
       ),
+    );
+  }
+
+  Future deleteBook(
+      BuildContext context, BookListModel model, Book book) async {
+    try {
+      //TODO:TextFieldに入力されたらFire Storeに本を追加する
+      await model.deleteBook(book);
+      //TODO:model.fetchBooks();で画面を更新。
+      await model.fetchBooks();
+      //TODO:awaitと入力することで順番に処理が走るので同時に発火してイベントを打ち消すのを防ぐことができる
+    } catch (e) {
+      //TODO:addBookToFirebase内でbookTitleがnilのときにエラー文をshowDialogとして出す
+      _showDialog(context, e.toString());
+    }
+  }
+
+  Future _showDialog(BuildContext context, String title) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
